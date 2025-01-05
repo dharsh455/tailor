d2e50728a6cf59e5eace40ff365972b4bdedde00
dharsh455 <dharshan103007@gmail.com>
2025-01-05 17:25:46 +0530

    Update cart.js

--- static/cart.js
+++ static/cart.js
@@ -1,1 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+    addToCartButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const productName = button.getAttribute("data-product");
+            const productPrice = button.getAttribute("data-price");
+            const productImage = button.parentElement.querySelector("img").src;
+
+            // Send data to the server
+            fetch("add_to_cart.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    product_name: productName,
+                    product_price: productPrice,
+                    product_image: productImage,
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.status === "success") {
+                        alert("Product added to cart!");
+                    } else {
+                        alert("Error: " + data.message);
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
+        });
+    });
+});
 
